65dea9aa90a463de01f0a43d94d87b871273c91c
Pablo CHERON <pcheron@bess-f2r1s9.clusters.42paris.fr>
2025-02-17 20:32:52 +0100

    chapter 1 ended: normalize + dot prod + cross prod

--- src/structure/vector.rs
+++ src/structure/vector.rs
@@ -11,14 +11,62 @@ impl Vector {
         Self { data }
     }
 
-    pub fn magnitude(self) -> f32 {
+    pub fn magnitude(&self) -> f32 {
         (self.data.x * self.data.x + self.data.y * self.data.y + self.data.z * self.data.z).sqrt()
+    }
+
+    pub fn normalize(&self) -> Vector {
+        let mag = self.magnitude();
+        if mag == 0.0 {
+            return Vector {
+                data : Tuple {
+                x: 1.0,
+                y: 1.0,
+                z: 1.0,
+                w: 1.0,
+                }
+            };
+        }
+        Vector {
+            data : Tuple {
+            x: self.data.x / mag,
+            y: self.data.y / mag,
+            z: self.data.z / mag,
+            w: 1.0,
+            }
+        }
+    }
+
+    pub fn dot(&self, other: &Self) -> f32 {
+        self.data.x * other.data.x +
+        self.data.y * other.data.y +
+        self.data.z * other.data.z
+    }
+
+    pub fn cross(&self, other: &Self) -> Self {
+        Self {
+            data: Tuple {
+                x: self.data.y * other.data.z - self.data.z * other.data.y,
+                y: self.data.z * other.data.x - self.data.x * other.data.z,
+                z: self.data.x * other.data.y - self.data.y * other.data.x,
+                w: 1.0,
+            }
+        }
     }
 }
 
 // -----------------------------------------------------------------
 // Operator overloading
 // -----------------------------------------------------------------
+
+impl PartialEq for Vector {
+    fn eq(&self, other: &Self) -> bool {
+        (self.data.x - other.data.x).abs() < f32::EPSILON &&
+        (self.data.y - other.data.y).abs() < f32::EPSILON &&
+        (self.data.z - other.data.z).abs() < f32::EPSILON &&
+        (self.data.w - other.data.w).abs() < f32::EPSILON
+    }
+}
 
 impl Add for Vector {
     type Output = Self;
--- tests/test_vector.rs
+++ tests/test_vector.rs
@@ -1,4 +1,6 @@
 use rt::Vector;
+// use rt::{Vector, are_almost_equal};
+use rt::utils::are_almost_equal;
 
 #[test]
 fn test_create_vector() {
@@ -76,4 +78,57 @@ fn test_magnitude_of_vector_5() {
     let magn_va = v_a.magnitude();
     let val = 14.0_f32;
     assert_eq!(magn_va, val.sqrt());
+}
+
+#[test]
+fn test_normalize_vector_1() {
+    let v_a = Vector::new([4.0, 0.0, 0.0]);
+
+    let norm_va = v_a.normalize();
+    let val = Vector::new([1.0, 0.0, 0.0]);
+    assert_eq!(norm_va.data.x, val.data.x);
+    assert_eq!(norm_va.data.y, val.data.y);
+    assert_eq!(norm_va.data.z, val.data.z);
+}
+
+#[test]
+fn test_normalize_vector_2() {
+    let v_a = Vector::new([1.0, 2.0, 3.0]);
+
+    let norm_va = v_a.normalize();
+    let val = Vector::new([0.26726, 0.53452, 0.80178]);
+    assert_eq!(are_almost_equal(norm_va.data.x, val.data.x), true);
+    assert_eq!(are_almost_equal(norm_va.data.y, val.data.y), true);
+    assert_eq!(are_almost_equal(norm_va.data.z, val.data.z), true);
+}
+
+#[test]
+fn test_dot_product_vector() {
+    let v_a = Vector::new([1.0, 2.0, 3.0]);
+    let v_b = Vector::new([2.0, 3.0, 4.0]);
+
+    let dot_prod = v_a.dot(&v_b);
+    assert_eq!(dot_prod, 20.0);
+}
+
+#[test]
+fn test_cross_product_vector_1() {
+    let v_a = Vector::new([1.0, 2.0, 3.0]);
+    let v_b = Vector::new([2.0, 3.0, 4.0]);
+
+    let result = v_a.cross(&v_b);
+    assert_eq!(result.data.x, -1.0);
+    assert_eq!(result.data.y, 2.0);
+    assert_eq!(result.data.z, -1.0);
+}
+
+#[test]
+fn test_cross_product_vector_2() {
+    let v_a = Vector::new([1.0, 2.0, 3.0]);
+    let v_b = Vector::new([2.0, 3.0, 4.0]);
+
+    let result = v_b.cross(&v_a);
+    assert_eq!(result.data.x, 1.0);
+    assert_eq!(result.data.y, -2.0);
+    assert_eq!(result.data.z, 1.0);
 }
