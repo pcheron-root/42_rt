718b4e0f35b5e732b57b8becdbb74bb86e7affb4
Pradene <liam.pradene@gmail.com>
2025-03-03 16:44:13 +0100

    Adding plane intersection test

--- src/structure/canvas.rs
+++ src/structure/canvas.rs
@@ -103,23 +103,23 @@ impl Canvas {
         let ambient = effective_color * material.ambient;
         let light_dot_normal = lightv.dot(normalv);
 
-        let diffuse;
-        let specular;
         if light_dot_normal < 0. {
-            diffuse = Color::new(0., 0., 0.);
+            return ambient;
+        }
+        
+        let specular;
+        let diffuse = effective_color * material.diffuse * light_dot_normal;
+        
+        let reflectv = (-lightv).reflect(normalv);
+        let reflect_dot_eye = reflectv.dot(eyev);
+        
+        if reflect_dot_eye <= 0. {
             specular = Color::new(0., 0., 0.);
         } else {
-            diffuse = effective_color * material.diffuse * light_dot_normal;
-            let reflectv = (-lightv).reflect(normalv);
-            let reflect_dot_eye = reflectv.dot(eyev);
-
-            if reflect_dot_eye <= 0. {
-                specular = Color::new(0., 0., 0.);
-            } else {
-                let factor = reflect_dot_eye.powf(material.shininess);
-                specular = light.intensity * material.specular * factor;
-            }
+            let factor = reflect_dot_eye.powf(material.shininess);
+            specular = light.intensity * material.specular * factor;
         }
+
         ambient + diffuse + specular
     }
 
@@ -136,23 +136,23 @@ impl Canvas {
         let ambient = effective_color * material.ambient;
         let light_dot_normal = lightv.dot(normalv);
 
-        let diffuse;
-        let specular;
         if light_dot_normal < 0. {
-            diffuse = Color::new(0., 0., 0.);
+            return ambient;
+        }
+        
+        let specular;
+        let diffuse = effective_color * material.diffuse * light_dot_normal;
+        
+        let reflectv = (-lightv).reflect(normalv);
+        let reflect_dot_eye = reflectv.dot(eyev);
+        
+        if reflect_dot_eye <= 0. {
             specular = Color::new(0., 0., 0.);
         } else {
-            diffuse = effective_color * material.diffuse * light_dot_normal;
-            let reflectv = (-lightv).reflect(normalv);
-            let reflect_dot_eye = reflectv.dot(eyev);
-
-            if reflect_dot_eye <= 0. {
-                specular = Color::new(0., 0., 0.);
-            } else {
-                let factor = reflect_dot_eye.powf(material.shininess);
-                specular = light.intensity * material.specular * factor;
-            }
+            let factor = reflect_dot_eye.powf(material.shininess);
+            specular = light.intensity * material.specular * factor;
         }
+
         ambient + diffuse + specular
     }
 }
---
+++ tests/test_plane.rs
@@ -0,0 +1,54 @@
+#[cfg(test)]
+mod tests {
+    use rt::{Intersect, Object, Plane, Point, Ray, Shape, Vector};
+
+    #[test]
+    fn test_plane_intersection_from_above() {
+        let plane = Object::new(Shape::Plane(Plane::new()));
+        let ray = Ray::new(Point::new(0., 1., 0.), Vector::new(0., -1., 0.));
+
+        let hit = plane.intersect(ray);
+
+        assert!(hit.is_some(), "Ray must hit plane");
+        
+        let hit = hit.unwrap();
+        assert_eq!(hit.t, 1.);
+    }
+
+    #[test]
+    fn test_plane_intersection_from_below() {
+        let plane = Object::new(Shape::Plane(Plane::new()));
+        let ray = Ray::new(Point::new(0., -1., 0.), Vector::new(0., 1., 0.));
+
+        let hit = plane.intersect(ray);
+
+        assert!(hit.is_some(), "Ray must hit plane");
+        
+        let hit = hit.unwrap();
+        assert_eq!(hit.t, 1.);
+    }
+
+    #[test]
+    fn test_plane_no_intersection() {
+        let plane = Object::new(Shape::Plane(Plane::new()));
+        let ray = Ray::new(Point::new(0., 10., 0.), Vector::new(0., 0., 1.));
+
+        let hit = plane.intersect(ray);
+
+        assert!(hit.is_none(), "Ray must not hit plane");        
+    }
+
+    #[test]
+    fn test_plane_normal() {
+        let plane = Plane::new();
+
+        let n1 = plane.normal_at(Point::new(0., 0., 0.));
+        let n2 = plane.normal_at(Point::new(10., 0., -10.));
+        let n3 = plane.normal_at(Point::new(-5., 0., 150.));
+    
+        let normal = Vector::new(0., 1., 0.);
+        assert_eq!(n1, normal);
+        assert_eq!(n2, normal);
+        assert_eq!(n3, normal);
+    }
+}
